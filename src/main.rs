724ef0bdcb2b6759322fed7bfc101c56548320d9
Dennis Durling <djdtahoe@gmail.com>
2025-10-25 23:39:10 -0700

    boy that was not fun but we got it to force IPv4 or 'system settings'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use reqwest::{Client, ClientBuilder, Error, Response};
+use reqwest_hickory_resolver::HickoryResolver;
 use serde_json::{Result, Value};
 use std::env;
+use std::sync::Arc;
 
 /*
 Keys we care about in the api return for single items
@@ -22,19 +24,24 @@ We should have a set of types that lets us represent these parts at least.
 */
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> reqwest::Result<()> {
     dotenv::dotenv().ok();
+
+    let mut builder = ClientBuilder::new();
+    builder = builder.dns_resolver(Arc::new(HickoryResolver::default()));
+    let test_client = builder.build();
+
+    // let test_client = Arc::new(Client::builder().dns_resolver(resolver).build());
     let api_key = std::env::var("API_KEY").expect("Need to have API_KEY set in .env");
-    let test_client = Client::new();
     let api_result = test_client
+        .expect("lb")
         .get(format!(
-            "https://vapi2.netactuate.com/api/cloud/servers?key={}",
-            api_key
+            "https://vapi2.netactuate.com/api/cloud/servers?key={api_key}"
         ))
         .send()
-        .await;
-    let res_str = api_result.expect("bleh").text().await;
-    let res_json: Value = serde_json::from_str(&res_str.expect("blah")).expect("blah");
-    println!("{:#}", res_json);
+        .await?;
+    let res_str = api_result.text().await?;
+    let res_json: Value = serde_json::from_str(&res_str).expect("blah");
+    println!("{res_json:#}");
     Ok(())
 }
