aaf796539c6db2dcde5d3497c64b385e12284fe4
Dennis Durling <djdtahoe@gmail.com>
2025-11-01 01:37:58 -0700

    fixed up error handling a bit, no more use of unwrap for the most part. undid some serde renames

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,8 @@ async fn main() -> reqwest::Result<()> {
 
     if mbpkgid > 0 {
         // print basic server info
-        let srv_result = na_client.get_server(mbpkgid).await;
-        let srv = srv_result.unwrap();
+        let srv_result = na_client.get_server(mbpkgid).await?;
+        let srv = srv_result;
         println!(
             "Package: {}, fqdn: {}, mbpkgid: {}",
             srv.domu_package, srv.fqdn, srv.mbpkgid
@@ -53,8 +53,8 @@ async fn main() -> reqwest::Result<()> {
 
         println!();
         // print jobs
-        let jobs_result = na_client.get_jobs(mbpkgid).await;
-        let jobs = jobs_result.unwrap();
+        let jobs_result = na_client.get_jobs(mbpkgid).await?;
+        let jobs = jobs_result;
         for job in jobs {
             println!(
                 "Inserted: {}, Status: {}, command: {}",
@@ -64,8 +64,8 @@ async fn main() -> reqwest::Result<()> {
 
         println!();
         // print IPv4 Addresses
-        let ipv4_result = na_client.get_ipv4(mbpkgid).await;
-        let ipv4s = ipv4_result.unwrap();
+        let ipv4_result = na_client.get_ipv4(mbpkgid).await?;
+        let ipv4s = ipv4_result;
         for ipv4 in ipv4s {
             println!(
                 "Reverse: {}, IP: {}, Gateway: {}",
@@ -75,8 +75,8 @@ async fn main() -> reqwest::Result<()> {
 
         println!();
         // print IPv6 Addresses
-        let ipv6_result = na_client.get_ipv6(mbpkgid).await;
-        let ipv6s = ipv6_result.unwrap();
+        let ipv6_result = na_client.get_ipv6(mbpkgid).await?;
+        let ipv6s = ipv6_result;
         for ipv6 in ipv6s {
             println!(
                 "Reverse: {}, IP: {}, Gateway: {}",
@@ -86,36 +86,36 @@ async fn main() -> reqwest::Result<()> {
 
         println!();
         // print server status, very unverbose
-        let status_result = na_client.get_status(mbpkgid).await;
-        let stat = status_result.unwrap();
+        let status_result = na_client.get_status(mbpkgid).await?;
+        let stat = status_result;
         println!("Status: {}", stat.status);
     } else {
-        let srvrs_result = na_client.get_servers().await;
-        let srvrs = srvrs_result.unwrap();
+        let srvrs_result = na_client.get_servers().await?;
+        let srvrs = srvrs_result;
         for srvr in srvrs {
             println!("fqdn: {}, mbpkgid: {}", srvr.fqdn, srvr.mbpkgid);
         }
 
         println!();
         // list locations
-        let locs_result = na_client.get_locations().await;
-        let locs = locs_result.unwrap();
+        let locs_result = na_client.get_locations().await?;
+        let locs = locs_result;
         for loc in locs {
             println!("Name: {}, Continent: {}", loc.name, loc.continent);
         }
 
         println!();
         // list packages
-        let pkgs_result = na_client.get_packages().await;
-        let pkgs = pkgs_result.unwrap();
+        let pkgs_result = na_client.get_packages().await?;
+        let pkgs = pkgs_result;
         for pkg in pkgs {
             println!("Name: {}, Continent: {}", pkg.name, pkg.city);
         }
 
         println!();
         // list images
-        let imgs_result = na_client.get_images().await;
-        let imgs = imgs_result.unwrap();
+        let imgs_result = na_client.get_images().await?;
+        let imgs = imgs_result;
         for img in imgs {
             println!(
                 "ID: {}, Size: {}, Name: {}",
