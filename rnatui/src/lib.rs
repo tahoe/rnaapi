a85f494771fa2f6bee210cfa8ef1098028c8bee7
Dennis Durling <djdtahoe@gmail.com>
2026-02-10 22:24:35 -0800

    moved the command configuration into lib.rs

--- rnatui/src/lib.rs
+++ rnatui/src/lib.rs
@@ -1,3 +1,5 @@
+use clap::{Parser, Subcommand};
+use clap_complete::Shell;
 use std::io::BufRead;
 use std::{env, fs, io, path};
 
@@ -22,4 +24,105 @@ pub fn get_pub_key(filepath: &str) -> Option<String> {
     };
 
     Some(found_key?.to_string())
+}
+
+///
+/// This is the CLI Args struct
+///
+#[derive(Parser, Debug)]
+#[command(version, about)]
+#[command(arg_required_else_help(true))]
+pub struct Cli {
+    #[command(subcommand)]
+    pub cmd: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Get {
+        #[command(subcommand)]
+        cmd: GetCommands,
+    },
+    /// generate completions
+    #[command(visible_alias = "gen")]
+    GenerateCompletions { shell: Shell },
+}
+
+#[derive(Subcommand, Debug)]
+pub enum GetCommands {
+    /// Server subcommands
+    #[command(visible_alias = "srv")]
+    Server {
+        // -i argument for picking an mbpkgid
+        #[arg(short, long, default_value_t = 0)]
+        mbpkgid: u32,
+    },
+
+    /// DNS subcommands
+    Dns {
+        // -i argument for picking a dns zone
+        #[arg(short, long, default_value_t = 0)]
+        id: u32,
+    },
+
+    /// SSh subcommands
+    Ssh {
+        // -i argument for ssh keyid
+        #[arg(short, long, default_value_t = 0)]
+        id: u32,
+    },
+
+    /// Job subcommands
+    Job {
+        // -i argument for picking a Job
+        #[arg(short, long)]
+        mbpkgid: u32,
+        #[arg(short, long, default_value_t = 0)]
+        jobid: u32,
+    },
+
+    /// IPs subcommands
+    Ip {
+        // --proto argument (-p) for 4 or 6
+        // default to 4
+        #[arg(short, long)]
+        mbpkgid: u32,
+    },
+
+    /// Monthly Bandwidth subcommands
+    #[command(visible_alias = "bw")]
+    Bandwidth {
+        #[arg(short, long)]
+        mbpkgid: u32,
+    },
+
+    /// Invoices subcommands
+    #[command(visible_alias = "inv")]
+    Invoice {
+        // -i argument for number to display
+        #[arg(short, long, default_value_t = 5)]
+        count: usize,
+    },
+
+    /// Invoices subcommands
+    #[command(visible_alias = "sz")]
+    Sizes {
+        // -i argument for number to display
+        #[arg(short, long)]
+        id: u32,
+    },
+
+    /// Location subcommands
+    #[command(visible_alias = "loc")]
+    Location {},
+
+    /// Images subcommands
+    #[command(visible_alias = "img")]
+    Image {},
+
+    /// Account subcommands
+    Account {},
+
+    /// Get Pub Key from file
+    Pubkey {},
 }
--- rnatui/src/main.rs
+++ rnatui/src/main.rs
@@ -30,14 +30,14 @@
 // under the GNU General Public License v3.0
 use anyhow::Result;
 use chrono::NaiveDate;
-use clap::CommandFactory;
-use clap::{Parser, Subcommand};
-use clap_complete::{Shell, generate};
+use clap::{CommandFactory, Parser};
+use clap_complete::generate;
 use rnaapi::NaClient;
 use rnaapi::config::Settings;
 use rnaapi::endpoints;
 use rnaapi::{EndpointGetAll, EndpointGetArgs, EndpointGetOne};
 use rnatui::get_pub_key;
+use rnatui::{Cli, Commands, GetCommands};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -455,105 +455,4 @@ async fn main() -> Result<()> {
         }
     }
     Ok(())
-}
-
-///
-/// This is the CLI Args struct
-///
-#[derive(Parser, Debug)]
-#[command(version, about)]
-#[command(arg_required_else_help(true))]
-struct Cli {
-    #[command(subcommand)]
-    cmd: Option<Commands>,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    Get {
-        #[command(subcommand)]
-        cmd: GetCommands,
-    },
-    /// generate completions
-    #[command(visible_alias = "gen")]
-    GenerateCompletions { shell: Shell },
-}
-
-#[derive(Subcommand, Debug)]
-enum GetCommands {
-    /// Server subcommands
-    #[command(visible_alias = "srv")]
-    Server {
-        // -i argument for picking an mbpkgid
-        #[arg(short, long, default_value_t = 0)]
-        mbpkgid: u32,
-    },
-
-    /// DNS subcommands
-    Dns {
-        // -i argument for picking a dns zone
-        #[arg(short, long, default_value_t = 0)]
-        id: u32,
-    },
-
-    /// SSh subcommands
-    Ssh {
-        // -i argument for ssh keyid
-        #[arg(short, long, default_value_t = 0)]
-        id: u32,
-    },
-
-    /// Job subcommands
-    Job {
-        // -i argument for picking a Job
-        #[arg(short, long)]
-        mbpkgid: u32,
-        #[arg(short, long, default_value_t = 0)]
-        jobid: u32,
-    },
-
-    /// IPs subcommands
-    Ip {
-        // --proto argument (-p) for 4 or 6
-        // default to 4
-        #[arg(short, long)]
-        mbpkgid: u32,
-    },
-
-    /// Monthly Bandwidth subcommands
-    #[command(visible_alias = "bw")]
-    Bandwidth {
-        #[arg(short, long)]
-        mbpkgid: u32,
-    },
-
-    /// Invoices subcommands
-    #[command(visible_alias = "inv")]
-    Invoice {
-        // -i argument for number to display
-        #[arg(short, long, default_value_t = 5)]
-        count: usize,
-    },
-
-    /// Invoices subcommands
-    #[command(visible_alias = "sz")]
-    Sizes {
-        // -i argument for number to display
-        #[arg(short, long)]
-        id: u32,
-    },
-
-    /// Location subcommands
-    #[command(visible_alias = "loc")]
-    Location {},
-
-    /// Images subcommands
-    #[command(visible_alias = "img")]
-    Image {},
-
-    /// Account subcommands
-    Account {},
-
-    /// Get Pub Key from file
-    Pubkey {},
 }
